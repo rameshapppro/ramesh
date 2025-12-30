655ce16e9052c65ef664514793446b16ed0000be
AndroidProCode <ramesh.apppro@gmail.com>
2025-12-30 10:23:09 +0530

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,12 +1,14 @@
-import type { MetadataRoute } from 'next';
+import type { MetadataRoute } from "next";
+
+export const dynamic = "force-static";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    return [
-        {
-            url: 'https://me.toinfinite.dev',
-            lastModified: new Date(),
-            changeFrequency: 'monthly',
-            priority: 1,
-        },
-    ];
+  return [
+    {
+      url: "https://rameshapppro.github.io/ramesh",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+  ];
 }
