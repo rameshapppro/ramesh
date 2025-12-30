41360c8cd8d9a27494e5220a1339f5f90d1e2853
Ramesh R <rameshr@Mac.lan>
2025-12-30 17:43:12 +0530

    Layout width and height changed

--- app/projects/[slug]/_components/ProjectDetails.tsx
+++ app/projects/[slug]/_components/ProjectDetails.tsx
@@ -176,28 +176,35 @@ const ProjectDetails = ({ project }: Props) => {
                 </div>
 
                 <div
-                    className="fade-in-later relative flex flex-col gap-2 max-w-[800px] mx-auto"
+                    className="fade-in-later relative flex flex-col gap-2 max-w-[500px] mx-auto"
                     id="images"
                 >
                     {project.images.map((image) => (
-                        <div
-                            key={image}
-                            className="group relative w-full aspect-[750/400] bg-background-light"
-                            style={{
-                                backgroundImage: `url(${image})`,
-                                backgroundSize: 'cover',
-                                backgroundPosition: 'center 50%',
-                                backgroundRepeat: 'no-repeat',
-                            }}
-                        >
-                            <a
-                                href={image}
-                                target="_blank"
-                                className="absolute top-4 right-4 bg-background/70 text-foreground size-12 inline-flex justify-center items-center transition-all opacity-0 hover:bg-primary hover:text-primary-foreground group-hover:opacity-100"
-                            >
-                                <ExternalLink />
-                            </a>
-                        </div>
+                      <div
+    key={image}
+    className="
+        group
+        relative
+        w-[350px]
+        h-[600px]
+        bg-background-light
+        overflow-hidden
+    "
+    style={{
+        backgroundImage: `url(${image})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center 70%',
+        backgroundRepeat: 'no-repeat',
+    }}
+>
+    <a
+        href={image}
+        target="_blank"
+        className="absolute top-4 right-4 bg-background/70 text-foreground size-12 inline-flex justify-center items-center transition-all opacity-0 hover:bg-primary hover:text-primary-foreground group-hover:opacity-100"
+    >
+        <ExternalLink />
+    </a>
+</div>
                     ))} 
                 </div>
             </div>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -32,7 +32,7 @@ const Footer = async () => {
 
                 <div className="">
                     <a
-                        href="https://github.com/Tajmirul/portfolio-2.0"
+                        href="https://github.com/rameshapppro"
                         target="_blank"
                         className="leading-none text-muted-foreground hover:underline hover:text-white"
                     >
